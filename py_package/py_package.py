7534312e70fa8f6792c456ecd112e227238759a3
bohachu <cbh@cameo.tw>
2023-02-22 10:07:04 +0800

    Update py_package.py

--- py_package/py_package.py
+++ py_package/py_package.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-import json
 import sys
 
-
-
 if __name__ == '__main__':
-    data = json.load(sys.stdin)
-    param1 = data.get('param1')
-    param2 = data.get('param2')
-    print(f'param1={param1}, param2={param2}')
+    print('hi')
+    
